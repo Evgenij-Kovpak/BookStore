35ee3f448b658a451708d819ff324be36d8aad74
DESKTOP-QSRMGKE\Win10 <evgenijj1982@gmail.com>
2023-07-25 18:42:35 +0300

    Add project files.

---
+++ BookStore/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = null!;
+}
---
+++ BookStore/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int Count { get; set; }
+    public decimal Price { get; set; }
+    public Book Book { get; set; }
+}
